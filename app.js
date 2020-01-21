0b9f6ef251f43af4e2d43a60560919f36603c421
arindamad <32026868+arindamad@users.noreply.github.com>
2020-01-21 19:13:28 +0530

    update with fixed bug

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ $(document).ready(function(){
         toBetrns = 0;
         var betweenAngle =  360 / 12;
         var ranNum = Math.floor(Math.random() * totalDiv);
-        console.log(ranNum);
+
+        var existingRotate = $(".spinnerImg")[0].style.transform.split("(")[1].split("d")[0];
+        existingRotate =  parseInt(existingRotate); 
+        console.log(existingRotate, ranNum);
+        if(existingRotate !==0){
+            var toBeRotate =parseInt(existingRotate) % 360 - 15;
+            console.log(toBeRotate);
+            toBeRotate = existingRotate + (360*7) - (360 + toBeRotate);
+
+
+            var sum = 0;
+            
+        }else{
+            var toBeRotate = (360*7);  
+
+            var sum = (betweenAngle/2);
+
+        }       
+        
         // ranNum = 0;
 
-        var toBetrns =  (ranNum * betweenAngle) - (betweenAngle/2);
-
-        $(this).css({"transform": "rotate("+((360*5)- toBetrns)+"deg)"});
+        var toBetrns =  (ranNum * betweenAngle) - sum;
+        $(this).css({"transform": "rotate("+( toBeRotate - toBetrns)+"deg)"});
     });
 });
